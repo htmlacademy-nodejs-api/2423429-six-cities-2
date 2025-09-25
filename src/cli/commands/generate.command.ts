1e128bc4c4d006e493e902ac7182d098252ed1d0
Кирилл <oniblekir@gmail.com>
2025-09-25 17:54:42 +0300

    2.11. Добавит генератор в команду GenerateCommand.

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,6 +1,8 @@
 import got from 'got';
 import { Command } from './command.interface.js';
 import { MockServerData } from '../../shared/types/index.js';
+import { TSVOfferGenerator } from '../../shared/libs/offer-generator/tsv-offer-generator.js';
+import { appendFile } from 'node:fs/promises';
 
 export class GenerateCommand implements Command {
   private initialData: MockServerData;
@@ -10,6 +12,17 @@ export class GenerateCommand implements Command {
       this.initialData = await got.get(url).json();
     } catch {
       throw new Error(`Can't loa data from ${url}`);
+    }
+  }
+
+  private async write(filepath: string, offerCount: number) {
+    const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    for (let i = 0; i < offerCount; i++) {
+      await appendFile(
+        filepath,
+        `${tsvOfferGenerator.generate()}\n`,
+        { encoding: 'utf8'}
+      );
     }
   }
 
@@ -23,6 +36,8 @@ export class GenerateCommand implements Command {
 
     try {
       await this.load(url);
+      await this.write(filepath, offerCount);
+      console.info(`File ${filepath} was created!`);
     } catch (error: unknown) {
       console.error('Can\'t generate data');
 
