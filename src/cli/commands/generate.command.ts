a70f9b7d035cc6a75c2ff571649e5ad0c9ce4b0a
Кирилл <oniblekir@gmail.com>
2025-09-25 16:43:03 +0300

    npm i  -дома

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,16 +1,16 @@
-import { Command } from "./command.interface.js";
+import { Command } from './command.interface.js';
 
 
 export class GenerateCommand implements Command {
-    public getName(): string {
-        return '--generate';
-    }
+  public getName(): string {
+    return '--generate';
+  }
 
-    public execute(...parameters: string[]): void {
-        const [count, filepath, url] = parameters;
-        const offerCount = Number.parseInt(count, 10);
+  public execute(...parameters: string[]): void {
+    const [count, filepath, url] = parameters;
+    const offerCount = Number.parseInt(count, 10);
 
-          // Код для получения данных с сервера.
-          // Формирование объявлений.
-    }
+    // Код для получения данных с сервера.
+    // Формирование объявлений.
+  }
 }
