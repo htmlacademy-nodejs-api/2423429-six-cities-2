3a4fca61ede1526927008a137b495dc740f6b8cc
Kirill <oniblekir@gmail.com>
2025-11-27 11:26:32 +0300

    3.26. Настроит контейнер. К счастью, мы можем влиять на то, как `inversify` создаёт новые экземпляры классов.
    Мы можем явно сказать, что такой-то класс должен быть в одном экземпляре. То есть является синглтоном.
    
    Откроем `main.rest.ts` и добавим вызов метода `inSingletonScope` всем зависимостям.
    
    Попробуйте перезапустить приложение и убедиться, что теперь сообщение о создании логера отображается один раз.

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -7,9 +7,9 @@ import { Config, RestSchema } from './shared/libs/config/index.js';
 
 async function bootstrap() {
   const container = new Container();
-  container.bind<RestApplication>(Component.RestApplication).to(RestApplication);
-  container.bind<Logger>(Component.Logger).to(PinoLogger);
-  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig);
+  container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
+  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
+  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
 
   const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
