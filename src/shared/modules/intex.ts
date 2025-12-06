e193f75210b2ba867c0d851ad63b504fb8e1059f
Dkey51 <oniblekir@gmail.com>
2025-12-06 16:21:47 +0300

    offer.entity.ts

--- src/shared/modules/intex.ts
+++ src/shared/modules/intex.ts
@@ -1,2 +1,4 @@
-export * from './user/user.entity.js';
 export * from './user/dto/create-user.dto.js';
+export * from './user/user.entity.js';
+export * from './user/user.service.js';
+export * from './offer/offer.entity.js';
